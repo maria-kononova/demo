5092e5601cce01ec75575a32c61a0159892e1b79
Lidia <lidia.samorukova@yandex.ru>
2024-03-01 22:35:51 +0300

    add style for field and button

--- resume/forms.py
+++ resume/forms.py
@@ -32,7 +32,10 @@ EDUCATION_LEVEL_CHOICES = (
 )
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={"class": "myfield"}))
+    email = forms.EmailField(label='Почта', widget=forms.EmailInput(attrs={"class":"myfield field-email"}))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={"class": "myfield"}))
+    password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput(attrs={"class": "myfield"}))
 
     class Meta:
         model = User
@@ -40,20 +43,20 @@ class UserRegistrationForm(UserCreationForm):
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(label='Username')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={"class":"myfield"}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={"class":"myfield"}))
 
 
 class TestForm(forms.Form):
-    title = forms.CharField(label='Название резюме', max_length=45)
-    surname = forms.CharField(label='Фамилия', max_length=30)
-    name = forms.CharField(label='Имя', max_length=30)
-    middle_name = forms.CharField(label='Отчество', max_length=30, required=False)
-    photo = forms.ImageField(label='Фото')
-    birthday = forms.DateField(label='Дата рождения', widget=forms.SelectDateWidget(years=range(year, year-101, -1)))
-    gender = forms.ChoiceField(label='Пол', choices=GENDER_CHOICES)
-    phone = forms.CharField(label='Телефон', max_length=12)
-    email = forms.EmailField(label='Почта')
-    types_of_communication = forms.ChoiceField(label='Вид связи', choices=TYPES_OF_COMMUNICATION_CHOICES)
-    education_level = forms.ChoiceField(label='Уровень образования', choices=EDUCATION_LEVEL_CHOICES)
+    title = forms.CharField(label='Название резюме', max_length=45, widget=forms.TextInput(attrs={"class":"myfield"}))
+    surname = forms.CharField(label='Фамилия', max_length=30, widget=forms.TextInput(attrs={"class":"myfield"}))
+    name = forms.CharField(label='Имя', max_length=30, widget=forms.TextInput(attrs={"class":"myfield"}))
+    middle_name = forms.CharField(label='Отчество', max_length=30, required=False, widget=forms.TextInput(attrs={"class":"myfield"}))
+    photo = forms.FileField(label='Фото', widget=forms.FileInput(attrs={"class":"myfield_select"}))
+    birthday = forms.DateField(label='Дата рождения', widget=forms.SelectDateWidget(years=range(year, year-101, -1), attrs={"class":"myfield_select"}))
+    gender = forms.ChoiceField(label='Пол', choices=GENDER_CHOICES, widget=forms.Select(attrs={"class":"myfield_select"}))
+    phone = forms.CharField(label='Телефон', max_length=12, widget=forms.TextInput(attrs={"class":"myfield"}))
+    email = forms.EmailField(label='Почта', widget=forms.EmailInput(attrs={"class":"myfield field-email"}))
+    types_of_communication = forms.ChoiceField(label='Вид связи', choices=TYPES_OF_COMMUNICATION_CHOICES, widget=forms.Select(attrs={"class":"myfield_select"}))
+    education_level = forms.ChoiceField(label='Уровень образования', choices=EDUCATION_LEVEL_CHOICES, widget=forms.Select(attrs={"class":"myfield_select"}))
 
