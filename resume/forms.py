945351f307a564c6198a5df30570976fe76669bd
Lidia <lidia.samorukova@yandex.ru>
2024-03-01 21:04:31 +0300

    add fields

--- resume/forms.py
+++ resume/forms.py
@@ -1,8 +1,35 @@
+from datetime import timezone, datetime
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
+year = datetime.now().year
+
+# Выбор пола
+GENDER_CHOICES = (
+    (1, "Мужской"),
+    (2, "Женский"),
+)
+
+# Выбор вида связи
+TYPES_OF_COMMUNICATION_CHOICES = (
+    (1, "Телефон"),
+    (2, "Почта"),
+)
+
+# Выбор уровня образования
+EDUCATION_LEVEL_CHOICES = (
+    (1, "Среднее"),
+    (2, "Среднее специальное"),
+    (3, "Неоконченное высшее"),
+    (4, "Высшее"),
+    (5, "Бакалавр"),
+    (6, "Магистр"),
+    (7, "Кандидат наук"),
+    (8, "Доктор наук"),
+)
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -18,4 +45,14 @@ class UserLoginForm(AuthenticationForm):
 
 
 class TestForm(forms.Form):
-    title = forms.CharField(max_length=45)
+    title = forms.CharField(label='Название резюме', max_length=45)
+    surname = forms.CharField(label='Фамилия', max_length=30)
+    name = forms.CharField(label='Имя', max_length=30)
+    middle_name = forms.CharField(label='Отчество', max_length=30, required=False)
+    birthday = forms.DateField(label='Дата рождения', widget=forms.SelectDateWidget(years=range(year, year-101, -1)))
+    gender = forms.ChoiceField(label='Пол', choices=GENDER_CHOICES)
+    phone = forms.CharField(label='Телефон', max_length=12)
+    email = forms.EmailField(label='Почта')
+    types_of_communication = forms.ChoiceField(label='Вид связи', choices=TYPES_OF_COMMUNICATION_CHOICES)
+    education_level = forms.ChoiceField(label='Уровень образования', choices=EDUCATION_LEVEL_CHOICES)
+
