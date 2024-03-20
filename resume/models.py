d46467ef58e6bef9884d5032e5aa27929a47337b
Maria <515nonia515@gmail.com>
2024-03-20 18:34:45 +0300

    fix models

--- resume/models.py
+++ resume/models.py
@@ -1,48 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 # таблицы django
 class AboutJob(models.Model):
     id_about_job = models.IntegerField(db_column='ID_about_job', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey('Resume', models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey('Resume', models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     desired_position = models.CharField(db_column='Desired_position', max_length=45)  # Field name made lowercase.
-    desired_salary = models.IntegerField(db_column='Desired_salary', blank=True, null=True)  # Field name made lowercase.
-    currency = models.CharField(db_column='Currency', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    desired_salary = models.IntegerField(db_column='Desired_salary', blank=True,
+                                         null=True)  # Field name made lowercase.
+    currency = models.CharField(db_column='Currency', max_length=45, blank=True,
+                                null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
         db_table = 'about_job'
-
-
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
 
 
 class AuthUser(models.Model):
@@ -63,32 +36,11 @@ class AuthUser(models.Model):
         db_table = 'auth_user'
 
 
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
 class Busyness(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_about_job = models.ForeignKey(AboutJob, models.DO_NOTHING, db_column='ID_about_job')  # Field name made lowercase.
-    type_busyness = models.CharField(db_column='Type_busyness', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    id_about_job = models.ForeignKey(AboutJob, models.CASCADE, db_column='ID_about_job')  # Field name made lowercase.
+    type_busyness = models.CharField(db_column='Type_busyness', max_length=45, blank=True,
+                                     null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -97,7 +49,7 @@ class Busyness(models.Model):
 
 class Citizenship(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_student = models.ForeignKey('Students', models.DO_NOTHING, db_column='ID_student')  # Field name made lowercase.
+    id_student = models.ForeignKey('Students', models.CASCADE, db_column='ID_student')  # Field name made lowercase.
     country = models.CharField(db_column='Country', max_length=45)  # Field name made lowercase.
 
     class Meta:
@@ -107,7 +59,7 @@ class Citizenship(models.Model):
 
 class Courses(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey('Resume', models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey('Resume', models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     organization = models.CharField(db_column='Organization', max_length=45)  # Field name made lowercase.
     course_name = models.CharField(db_column='Course_name', max_length=45)  # Field name made lowercase.
     specialization = models.CharField(db_column='Specialization', max_length=45)  # Field name made lowercase.
@@ -118,64 +70,9 @@ class Courses(models.Model):
         db_table = 'courses'
 
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
-class DrivingLicense(models.Model):
-    id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey('Resume', models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
-    category = models.CharField(db_column='Category', max_length=5)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'driving_license'
-
-
 class EducationalInstitution(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey('Resume', models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey('Resume', models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     name_of_institution = models.CharField(db_column='Name_of_institution', max_length=45)  # Field name made lowercase.
     faculty = models.CharField(db_column='Faculty', max_length=45, blank=True, null=True)  # Field name made lowercase.
     specialization = models.CharField(db_column='Specialization', max_length=45)  # Field name made lowercase.
@@ -189,7 +86,7 @@ class EducationalInstitution(models.Model):
 
 class KeySkills(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey('Resume', models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey('Resume', models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     skill = models.CharField(db_column='Skill', max_length=45)  # Field name made lowercase.
 
     class Meta:
@@ -199,7 +96,7 @@ class KeySkills(models.Model):
 
 class Languages(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_student = models.ForeignKey('Students', models.DO_NOTHING, db_column='ID_student')  # Field name made lowercase.
+    id_student = models.ForeignKey('Students', models.CASCADE, db_column='ID_student')  # Field name made lowercase.
     language = models.CharField(db_column='Language', max_length=45)  # Field name made lowercase.
     proficiency_level = models.CharField(db_column='Proficiency_level', max_length=45)  # Field name made lowercase.
 
@@ -210,7 +107,7 @@ class Languages(models.Model):
 
 class Portfolio(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey('Resume', models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey('Resume', models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     photo = models.CharField(db_column='Photo', max_length=45)  # Field name made lowercase.
 
     class Meta:
@@ -220,13 +117,17 @@ class Portfolio(models.Model):
 
 class Resume(models.Model):
     id_resume = models.IntegerField(db_column='ID_resume', primary_key=True)  # Field name made lowercase.
-    id_student = models.ForeignKey('Students', models.DO_NOTHING, db_column='ID_student')  # Field name made lowercase.
-    description_skills = models.CharField(db_column='Description_skills', max_length=700, blank=True, null=True)  # Field name made lowercase.
+    id_student = models.ForeignKey('Students', models.CASCADE, db_column='ID_student')  # Field name made lowercase.
+    description_skills = models.CharField(db_column='Description_skills', max_length=700, blank=True,
+                                          null=True)  # Field name made lowercase.
     city = models.CharField(db_column='City', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    station_metro = models.CharField(db_column='Station_metro', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    possibility_of_transfer = models.CharField(db_column='Possibility_of_transfer', max_length=45)  # Field name made lowercase.
+    station_metro = models.CharField(db_column='Station_metro', max_length=45, blank=True,
+                                     null=True)  # Field name made lowercase.
+    possibility_of_transfer = models.CharField(db_column='Possibility_of_transfer',
+                                               max_length=45)  # Field name made lowercase.
     business_trips = models.CharField(db_column='Business_trips', max_length=45)  # Field name made lowercase.
-    desired_time_in_the_way = models.CharField(db_column='Desired_time_in_the_way', max_length=45)  # Field name made lowercase.
+    desired_time_in_the_way = models.CharField(db_column='Desired_time_in_the_way',
+                                               max_length=45)  # Field name made lowercase.
     availability_car = models.IntegerField(db_column='Availability_car')  # Field name made lowercase.
     locale_resume = models.CharField(db_column='Locale_resume', max_length=45)  # Field name made lowercase.
     date_of_creation = models.DateTimeField(db_column='Date_of_creation')  # Field name made lowercase.
@@ -248,7 +149,7 @@ class Role(models.Model):
 
 class Specialization(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_about_job = models.ForeignKey(AboutJob, models.DO_NOTHING, db_column='ID_about_job')  # Field name made lowercase.
+    id_about_job = models.ForeignKey(AboutJob, models.CASCADE, db_column='ID_about_job')  # Field name made lowercase.
     specialization = models.CharField(db_column='Specialization', max_length=45)  # Field name made lowercase.
 
     class Meta:
@@ -260,15 +161,17 @@ class Students(models.Model):
     id_student = models.IntegerField(db_column='ID_student', primary_key=True)  # Field name made lowercase.
     surname = models.CharField(db_column='Surname', max_length=30)  # Field name made lowercase.
     name = models.CharField(db_column='Name', max_length=30)  # Field name made lowercase.
-    middle_name = models.CharField(db_column='Middle_name', max_length=30, blank=True, null=True)  # Field name made lowercase.
+    middle_name = models.CharField(db_column='Middle_name', max_length=30, blank=True,
+                                   null=True)  # Field name made lowercase.
     birthdate = models.DateField(db_column='Birthdate', blank=True, null=True)  # Field name made lowercase.
     gender = models.CharField(db_column='Gender', max_length=7)  # Field name made lowercase.
-    photo = models.TextField(db_column='Photo', blank=True, null=True)  # Field name made lowercase.
+    photo = models.FileField(db_column='Photo', blank=True, null=True)  # Field name made lowercase.
     phone = models.CharField(db_column='Phone', max_length=12)  # Field name made lowercase.
     email = models.CharField(db_column='Email', max_length=45)  # Field name made lowercase.
-    types_of_communication = models.CharField(db_column='Types_of_communication', max_length=45)  # Field name made lowercase.
+    types_of_communication = models.CharField(db_column='Types_of_communication',
+                                              max_length=45)  # Field name made lowercase.
     education_level = models.CharField(db_column='Education_level', max_length=45)  # Field name made lowercase.
-    id_auth_user = models.ForeignKey(AuthUser, models.DO_NOTHING, db_column='ID_auth_user')  # Field name made lowercase.
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -285,7 +188,7 @@ class Test(models.Model):
 
 class TestsAndExams(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey(Resume, models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey(Resume, models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     organization = models.CharField(db_column='Organization', max_length=45)  # Field name made lowercase.
     course_name = models.CharField(db_column='Course_name', max_length=45)  # Field name made lowercase.
     specialization = models.CharField(db_column='Specialization', max_length=45)  # Field name made lowercase.
@@ -296,27 +199,20 @@ class TestsAndExams(models.Model):
         db_table = 'tests_and_exams'
 
 
-class User(models.Model):
-    id = models.IntegerField(primary_key=True)
-    login = models.CharField(max_length=45)
-    password = models.CharField(max_length=45)
-    role = models.ForeignKey(Role, models.DO_NOTHING, db_column='role')
-
-    class Meta:
-        managed = False
-        db_table = 'user'
-
-
 class WorkExperience(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_resume = models.ForeignKey(Resume, models.DO_NOTHING, db_column='ID_resume')  # Field name made lowercase.
+    id_resume = models.ForeignKey(Resume, models.CASCADE, db_column='ID_resume')  # Field name made lowercase.
     organization = models.CharField(db_column='Organization', max_length=45)  # Field name made lowercase.
-    location = models.CharField(db_column='Location', max_length=45, blank=True, null=True)  # Field name made lowercase.
-    industries = models.CharField(db_column='Industries', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    location = models.CharField(db_column='Location', max_length=45, blank=True,
+                                null=True)  # Field name made lowercase.
+    industries = models.CharField(db_column='Industries', max_length=45, blank=True,
+                                  null=True)  # Field name made lowercase.
     occupied_position = models.CharField(db_column='Occupied_position', max_length=45)  # Field name made lowercase.
     start_work = models.DateTimeField(db_column='Start_work')  # Field name made lowercase.
     end_work = models.DateTimeField(db_column='End_work')  # Field name made lowercase.
-    descriptionres_ponsibilities_and_achievements = models.CharField(db_column='Descriptionres_ponsibilities_and_achievements', max_length=500, blank=True, null=True)  # Field name made lowercase.
+    descriptionres_ponsibilities_and_achievements = models.CharField(
+        db_column='Descriptionres_ponsibilities_and_achievements', max_length=500, blank=True,
+        null=True)  # Field name made lowercase.
 
     class Meta:
         managed = False
@@ -325,7 +221,7 @@ class WorkExperience(models.Model):
 
 class WorkResolution(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_student = models.ForeignKey(Students, models.DO_NOTHING, db_column='ID_student')  # Field name made lowercase.
+    id_student = models.ForeignKey(Students, models.CASCADE, db_column='ID_student')  # Field name made lowercase.
     country = models.CharField(db_column='Country', max_length=45)  # Field name made lowercase.
 
     class Meta:
@@ -335,7 +231,7 @@ class WorkResolution(models.Model):
 
 class WorkTimetable(models.Model):
     id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
-    id_about_job = models.ForeignKey(AboutJob, models.DO_NOTHING, db_column='ID_about_job')  # Field name made lowercase.
+    id_about_job = models.ForeignKey(AboutJob, models.CASCADE, db_column='ID_about_job')  # Field name made lowercase.
     work_timetable = models.CharField(db_column='Work_timetable', max_length=45)  # Field name made lowercase.
 
     class Meta:
